62f5ac0777fb85236e0993692b7e934c546827c1
Andrew Grachov <grachovand@gmail.com>
2020-06-28 12:08:35 +0300

    homework №17 done

---
+++ Дз №17/script.js
@@ -0,0 +1,87 @@
+const URL = 'https://5dd3d5ba8b5e080014dc4bfa.mockapi.io/stickers';
+const DELETE_STICKER_CLASS = 'deleteSticker';
+const STICKER_BODY_CLASS = 'stickerBody';
+const STICKER_CLASS = '.sticker';
+
+const inputStickerBody = document.getElementById('inputStickerBody');
+const inputStickerTitle = document.getElementById('inputStickerTitle');
+const createStickerBtnEl = document.getElementById('createStickerBtn');
+const stickerListBlockEl = document.getElementById('stickerListBlock');
+const stickerTemplate = document.getElementById('stickerTemplate').innerHTML;
+
+createStickerBtnEl.addEventListener('click',oncreateStickerBtnElClick);
+stickerListBlockEl.addEventListener('click',onstickerListBlockElClick);
+stickerListBlockEl.addEventListener('blur',onstickerListBlockElBlure, true);
+
+let stickers = [];
+
+init();
+
+function init(){
+    getStickers();
+}
+function getStickers(){
+    fetch(URL)
+    .then((res) => res.json())
+    .then(setStickersList)
+    .then(renderStickersList);
+}
+function setStickersList(data){
+    return (stickers = data)
+}
+function renderStickersList(data){
+    console.log(data);
+    stickerListBlockEl.innerHTML = data.map(changeTemplate).join('');
+}
+function changeTemplate(data){
+    return stickerTemplate
+    .replace('{{id}}' ,data.id)
+    .replace('{{body}}' ,data.value)
+    .replace('{{title}}' ,data.description);
+}
+function oncreateStickerBtnElClick(){
+    const sticker = {description: inputStickerTitle.value, value: inputStickerBody.value}
+    fetch(URL, {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify(sticker),
+    })
+    .then((res) => res.json())
+    .then((data) =>{
+        stickers.push(data);
+        renderStickersList(stickers);
+    })
+
+}
+function onstickerListBlockElClick(e){
+    if(e.target.classList.contains(DELETE_STICKER_CLASS)){
+        deleteSticker(e.target.closest(STICKER_CLASS).dataset.id);
+    }
+}
+function deleteSticker(id){
+    fetch(`${URL}/${id}`,{
+        method: 'DELETE'
+    })
+    stickers = stickers.filter((sticker) => sticker.id !==id);
+    renderStickersList(stickers);
+
+}
+function onstickerListBlockElBlure(e){
+    if(e.target.classList.contains(STICKER_BODY_CLASS)){
+        changeStickerDescription(e.target.parentNode.dataset.id,e.target.value);
+    }
+}
+function changeStickerDescription(id, newValue){
+    let sticker = stickers.find((sticker) => sticker.id === id);
+    sticker.value = newValue;
+    fetch(`${URL}/${id}`,{
+        method:'PUT',
+        headers: {
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify(sticker),
+    })
+    renderStickersList(stickers);
+}
